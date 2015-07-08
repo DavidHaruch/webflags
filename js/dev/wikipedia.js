f8f2da0acc2a5e93cbd148dfee444438544fd30e
emphaticpuma <davidjharuch@gmail.com>
2015-07-08 17:33:04 -0400

    ui and styles improvements - added wikipedia button

---
+++ js/dev/wikipedia.js
@@ -0,0 +1,22 @@
+function wiki (name) {
+	
+	nametext = name;
+
+
+	find = ' ';
+	takeoutspacesregex = new RegExp(find, 'g');   // find string find globally throughout
+	name = name.replace(takeoutspacesregex, '_'); // replace find with selected character
+
+	url = "http://wikipedia.org/wiki/" + name;
+
+	wikiElem = document.createElement('a');
+	wikiElem.setAttribute('href', url);
+	wikiElem.className = 'button-flat';
+	wikiElem.textContent = 'wikipedia';
+
+	contentContainer = document.getElementById('nameDispCardContent'+i);
+	contentContainer.appendChild(wikiElem);
+
+	insertClearBoth(contentContainer);
+
+}
